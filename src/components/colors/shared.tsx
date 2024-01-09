b157a5ea5ef7673f7c3b3e63df5da7c0c0af2f0b
pridwigo <primantorodev@outlook.com>
2024-01-09 13:34:04 +0700

    add src

---
+++ src/components/colors/shared.tsx
@@ -0,0 +1,8 @@
+// import { Dimensions } from "react-native";
+// import styled from "styled-components";
+// import { colors } from "./colors";
+
+// const Container = styled.View`
+// flex: 1;
+// align-items
+// `;
